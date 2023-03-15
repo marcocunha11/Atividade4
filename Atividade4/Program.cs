3690ae475a723b353e5cd6f57709d819656ca372
Marco Henrique Cunha Dias <105382154+marcocunha11@users.noreply.github.com>
2023-03-15 19:33:43 -0300

    Add files via upload

---
+++ Atividade4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double bc, bc2;
+
+            //declarando um objeto para classe triangulo
+            triangulo t = new triangulo();            
+
+            Console.WriteLine("Digite o valor do lado A:");
+            t.setLadoA(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite o valor do lado B:");
+            t.setLadoB(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite o valor do lado C:");
+            t.setLadoC(double.Parse(Console.ReadLine()));
+
+            //CRIADO PARA SEPARAR
+            Console.WriteLine("");
+            //variaveis
+            bc = t.getLadoB() + t.getLadoC();
+            bc2 = Math.Pow(t.getLadoB(), 2) + Math.Pow(t.getLadoC(), 2);
+
+            if(t.getLadoA() >= bc)
+            {
+                Console.WriteLine("R: NÃO FORMA TRIANGULO");
+            }
+
+            else if(Math.Pow(t.getLadoA(), 2) == bc2)
+            {
+                Console.WriteLine("R: TRIANGULO RETANGULO");
+            }
+
+            else if (Math.Pow(t.getLadoA(), 2) > bc2)
+            {
+                Console.WriteLine("R: TRIANGULO OBTUSANGULO");
+            }
+
+            else if (Math.Pow(t.getLadoA(), 2) < bc2)
+            {
+                Console.WriteLine("R: TRIANGULO ACUTANGULO");
+            }
+
+            else if (t.getLadoA() == t.getLadoB() && t.getLadoB() == t.getLadoC())
+            {
+                Console.WriteLine("R: TRIANGULO EQUILATERO");
+            }
+
+            else
+            {
+                Console.WriteLine("R: TRIANGULO ISOSCELES");
+            }
+
+            
+
+
+
+        }
+    }
+}
---
+++ Atividade4/triangulo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4
+{
+    internal class triangulo
+    {
+        private double ladoA;
+        private double ladoB;
+        private double ladoC;
+
+        public triangulo() { }
+
+        public triangulo(double ladoA, double ladoB, double ladoC)
+        {
+            this.ladoA = ladoA;
+            this.ladoB = ladoB;
+            this.ladoC = ladoC;
+        }
+
+        public void setLadoA(double ladoA)
+        {
+            this.ladoA = ladoA;
+        }
+
+        public double getLadoA()
+        {
+            return this.ladoA;
+        }
+
+        public void setLadoB(double ladoB)
+        {
+            this.ladoB = ladoB;
+        }
+
+        public double getLadoB()
+        {
+            return this.ladoB;
+        }
+
+        public void setLadoC(double ladoC)
+        {
+            this.ladoC = ladoC;
+        }
+
+        public double getLadoC()
+        {
+            return this.ladoC;
+        }
+    }
+}
+
